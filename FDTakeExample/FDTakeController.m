faa4b0e62da3d1c2953d212004960d26eb945d6e
William Entriken <github.com@phor.net>
2012-09-30 19:56:56 -0400

    Badge update when loading

--- FDTakeExample/FDTakeController.m
+++ FDTakeExample/FDTakeController.m
@@ -8,7 +8,7 @@
 
 #import "FDTakeController.h"
 
-@interface FDTakeController() <UIActionSheetDelegate, UIAlertViewDelegate, UIImagePickerControllerDelegate>
+@interface FDTakeController() <UIActionSheetDelegate, UIAlertViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 @property (strong, nonatomic) NSMutableArray *sources;
 @property (strong, nonatomic) NSMutableArray *buttonTitles;
 @property (strong, nonatomic) UIActionSheet *actionSheet;
@@ -19,6 +19,96 @@
 @synthesize buttonTitles = _buttonTitles;
 @synthesize actionSheet = _actionSheet;
 @synthesize imagePicker = _imagePicker;
+
++ (UIImage*)imageWithImage:(UIImage*)sourceImage scaledToSizeWithSameAspectRatio:(CGSize)targetSize;
+{
+    CGSize imageSize = sourceImage.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    CGFloat targetWidth = targetSize.width;
+    CGFloat targetHeight = targetSize.height;
+    CGFloat scaleFactor = 0.0;
+    CGFloat scaledWidth = targetWidth;
+    CGFloat scaledHeight = targetHeight;
+    CGPoint thumbnailPoint = CGPointMake(0.0,0.0);
+    
+    if (CGSizeEqualToSize(imageSize, targetSize) == NO) {
+        CGFloat widthFactor = targetWidth / width;
+        CGFloat heightFactor = targetHeight / height;
+        
+        if (widthFactor > heightFactor) {
+            scaleFactor = widthFactor; // scale to fit height
+        }
+        else {
+            scaleFactor = heightFactor; // scale to fit width
+        }
+        
+        scaledWidth  = width * scaleFactor;
+        scaledHeight = height * scaleFactor;
+        
+        // center the image
+        if (widthFactor > heightFactor) {
+            thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5;
+        }
+        else if (widthFactor < heightFactor) {
+            thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
+        }
+    }
+    
+    CGImageRef imageRef = [sourceImage CGImage];
+    CGBitmapInfo bitmapInfo = CGImageGetBitmapInfo(imageRef);
+    CGColorSpaceRef colorSpaceInfo = CGImageGetColorSpace(imageRef);
+    
+    if (bitmapInfo == kCGImageAlphaNone) {
+        bitmapInfo = kCGImageAlphaNoneSkipLast;
+    }
+    
+    CGContextRef bitmap;
+    
+    if (sourceImage.imageOrientation == UIImageOrientationUp || sourceImage.imageOrientation == UIImageOrientationDown) {
+        bitmap = CGBitmapContextCreate(NULL, targetWidth, targetHeight, CGImageGetBitsPerComponent(imageRef), CGImageGetBytesPerRow(imageRef), colorSpaceInfo, bitmapInfo);
+        
+    } else {
+        bitmap = CGBitmapContextCreate(NULL, targetHeight, targetWidth, CGImageGetBitsPerComponent(imageRef), CGImageGetBytesPerRow(imageRef), colorSpaceInfo, bitmapInfo);
+        
+    }
+    
+    // In the right or left cases, we need to switch scaledWidth and scaledHeight,
+    // and also the thumbnail point
+    if (sourceImage.imageOrientation == UIImageOrientationLeft) {
+        thumbnailPoint = CGPointMake(thumbnailPoint.y, thumbnailPoint.x);
+        CGFloat oldScaledWidth = scaledWidth;
+        scaledWidth = scaledHeight;
+        scaledHeight = oldScaledWidth;
+        
+        CGContextRotateCTM (bitmap, M_PI/2);
+        CGContextTranslateCTM (bitmap, 0, -targetHeight);
+        
+    } else if (sourceImage.imageOrientation == UIImageOrientationRight) {
+        thumbnailPoint = CGPointMake(thumbnailPoint.y, thumbnailPoint.x);
+        CGFloat oldScaledWidth = scaledWidth;
+        scaledWidth = scaledHeight;
+        scaledHeight = oldScaledWidth;
+        
+        CGContextRotateCTM (bitmap, -M_PI/2);
+        CGContextTranslateCTM (bitmap, -targetWidth, 0);
+        
+    } else if (sourceImage.imageOrientation == UIImageOrientationUp) {
+        // NOTHING
+    } else if (sourceImage.imageOrientation == UIImageOrientationDown) {
+        CGContextTranslateCTM (bitmap, targetWidth, targetHeight);
+        CGContextRotateCTM (bitmap, M_PI);
+    }
+    
+    CGContextDrawImage(bitmap, CGRectMake(thumbnailPoint.x, thumbnailPoint.y, scaledWidth, scaledHeight), imageRef);
+    CGImageRef ref = CGBitmapContextCreateImage(bitmap);
+    UIImage* newImage = [UIImage imageWithCGImage:ref];
+    
+    CGContextRelease(bitmap);
+    CGImageRelease(ref);
+    
+    return newImage;
+}
 
 - (FDTakeController *)init
 {
@@ -65,16 +155,27 @@
                                                   cancelButtonTitle:nil
                                                   otherButtonTitles:nil];
         [alertView show];
-    }        
+    }
+}
+
+- (void)takeVideoOrChooseFromLibrary
+{
+#warning TODO
+}
+
+- (void)takePhotoOrVideoOrChooseFromLibrary
+{
+#warning TODO
 }
 
 #pragma mark - UIActionSheetDelegate
 
 - (void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex
 {
-    if (buttonIndex == self.actionSheet.cancelButtonIndex)
-        [self.delegate takeController:self didCancelAfterAttempting:NO];
-    else {
+    if (buttonIndex == self.actionSheet.cancelButtonIndex) {
+        if ([self.delegate respondsToSelector:@selector(takeController:didCancelAfterAttempting:)])
+            [self.delegate takeController:self didCancelAfterAttempting:NO];
+    } else {
         self.imagePicker.sourceType = [[self.sources objectAtIndex:buttonIndex] integerValue];
         [[[[UIApplication sharedApplication] keyWindow] rootViewController] presentViewController:self.imagePicker animated:YES completion:nil];
     }
@@ -84,21 +185,26 @@
 
 - (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
 {
-    [self.delegate takeController:self didCancelAfterAttempting:NO];
+    if ([self.delegate respondsToSelector:@selector(takeController:didCancelAfterAttempting:)])
+        [self.delegate takeController:self didCancelAfterAttempting:NO];
 }
 
 #pragma mark - UIImagePickerControllerDelegate
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
 {
-    [self.delegate takeController:self gotPhoto:[info objectForKey:@"UIImagePickerControllerOriginalImage"] withInfo:info];
-    [picker dismissModalViewControllerAnimated:YES];    
+    if ([info objectForKey:UIImagePickerControllerEditedImage])
+        [self.delegate takeController:self gotPhoto:[info objectForKey:@"UIImagePickerControllerEditedImage"] withInfo:info];
+    else
+        [self.delegate takeController:self gotPhoto:[info objectForKey:@"UIImagePickerControllerOriginalImage"] withInfo:info];
+    [picker dismissModalViewControllerAnimated:YES];
 }
 
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
 {
     [picker dismissModalViewControllerAnimated:YES];
-    [self.delegate takeController:self didCancelAfterAttempting:YES];
+    if ([self.delegate respondsToSelector:@selector(takeController:didCancelAfterAttempting:)])
+        [self.delegate takeController:self didCancelAfterAttempting:YES];
 }
 
 @end
