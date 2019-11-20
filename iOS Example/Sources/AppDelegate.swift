3963483c4093bd9c07e5698dca5df39942f4c5cc
Yaroslav Zhurakovskiy <yaroslav.zhurakovskiy@gmail.com>
2019-11-20 11:13:30 +0200

    Update project layout.

---
+++ iOS Example/Sources/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  iOS Example
+//
+//  Created by William Entriken on Sep 17, 2016.
+//  Copyright © 2016 William Entriken. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
